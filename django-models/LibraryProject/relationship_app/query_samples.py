e7487e4611fc5440dc75de888d0500ab24353671
Musungu2 <ruthmusungu2019@gmail.com>
2025-07-19 18:55:08 +0300

    added

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,7 +13,7 @@ from relationship_app.models import Author, Book, Library
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        books = author.books.all()
+        books = Book.objects.filter(author=author) 
         print(f"Books by {author.name}:")
         for book in books:
             print(f"- {book.title}")
