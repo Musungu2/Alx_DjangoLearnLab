4cc9938393c2b601c885f9a61359711dc2ae75e0
Musungu2 <ruthmusungu2019@gmail.com>
2025-07-25 18:32:00 +0300

    commit

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import CustomUser
+from accounts.models import CustomUser 
 
 # Custom admin configuration
 class BookAdmin(admin.ModelAdmin):
