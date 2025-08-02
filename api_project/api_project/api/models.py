ac479e08a0acb94dc05b8e36cb9d54215b863bf3
Musungu2 <ruthmusungu2019@gmail.com>
2025-08-02 18:48:09 +0300

    initial

--- api_project/api_project/api/models.py
+++ api_project/api_project/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-# Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
