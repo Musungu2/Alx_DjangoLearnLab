7a519c23e635f868066e0ca53ab687c1b2612cbd
Musungu2 <ruthmusungu2019@gmail.com>
2025-08-11 21:43:58 +0300

    added

--- django_blog/django_blog/django_blog/settings.py
+++ django_blog/django_blog/django_blog/settings.py
@@ -75,16 +75,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_db',
-        'USER': 'blog_user',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
