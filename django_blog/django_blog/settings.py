3424b17666f67e0c77ece252a75a0ca4e4c29383
Musungu2 <ruthmusungu2019@gmail.com>
2025-08-16 18:24:22 +0300

    added

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -121,3 +121,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+TEMPLATES = [
+    {
+        'DIRS': [BASE_DIR / 'templates'],
+    },
+]
+
