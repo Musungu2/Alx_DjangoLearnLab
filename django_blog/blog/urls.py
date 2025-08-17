c9f97ef1d0187d2c3542e7cff29ea3567c86bc7e
Musungu2 <ruthmusungu2019@gmail.com>
2025-08-17 19:19:53 +0300

    updated views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,3 +1,10 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+]
