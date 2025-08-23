20dcc24a016435067f516be68a517c02b3bc2fc1
Musungu2 <ruthmusungu2019@gmail.com>
2025-08-23 21:37:17 +0300

    done

---
+++ social_media_api/README.md
@@ -0,0 +1,19 @@
+## Posts
+
+* GET /api/posts/ → list all posts
+
+* POST /api/posts/ → create post (auth required)
+
+* PUT /api/posts/{id}/ → update own post
+
+* DELETE /api/posts/{id}/ → delete own post
+
+## Comments
+
+* GET /api/comments/ → list comments
+
+* POST /api/comments/ → create comment (auth required)
+
+* PUT /api/comments/{id}/ → edit own comment
+
+* DELETE /api/comments/{id}/ → delete own comment
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
